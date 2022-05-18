6ce27550605b994201d9883c80edc477b90278e4
Elian Paredes <elianparedes.dev@gmail.com>
2022-05-18 18:21:04 -0300

    feat: gdb support

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+if [[ "$1" = "gdb" ]]; then
+    echo "initializing qemu with gdb..."
+    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
+else 
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+fi
