c145990b76764ef8d3ff7ac177c23a0f750894ce
Juan Burda <jburda@itba.edu.ar>
2022-06-07 23:40:41 -0300

    fix: fibonacci bug

--- Userland/SampleCodeModule/programs/fibonacci.c
+++ Userland/SampleCodeModule/programs/fibonacci.c
@@ -13,6 +13,11 @@ void fibonacci(){
     int next= n1 + n2;
 
     while (1){
+        if (next <0){
+            n1=0;
+            n2=1;
+            next=n1+n2;
+        }
         printf("%d ", next);
         n1=n2;
         n2=next;
