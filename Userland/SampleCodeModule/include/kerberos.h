5c4ba664c1e78ed3666b7b8ee9753a229e5ae021
Elian Paredes <elianparedes.dev@gmail.com>
2022-06-16 23:44:21 -0300

    new: userland clear command and kerberos main screen

---
+++ Userland/SampleCodeModule/include/kerberos.h
@@ -0,0 +1,6 @@
+#ifndef _KERBEROS_H_
+#define _KERBEROS_H_
+
+void kerberos();
+
+#endif /* _KERBEROS_H_ */
---
+++ Userland/SampleCodeModule/programs/clear.c
@@ -0,0 +1,6 @@
+#include <clear.h>
+#include <kstdio.h>
+
+void clear(){
+    _clear_screen();
+}
